77c6c08507547b5f7175eec2247bf559acf625f4
Kruthika Krishnamurthy <boffti@gmail.com>
2020-04-12 19:13:13 +0530

    Updated DB URI

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///sfv_veggies.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///sfv_veggies.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_URI = 'postgres://pxvtdswblsuiga:52cd888957385327482ddfeeb0ea6546a507abab0b46b3cd91505a3523dba853@ec2-18-235-97-230.compute-1.amazonaws.com:5432/dag3cksb6esvq'
+SQLALCHEMY_DATABASE_URI = 'postgres://pxvtdswblsuiga:52cd888957385327482ddfeeb0ea6546a507abab0b46b3cd91505a3523dba853@ec2-18-235-97-230.compute-1.amazonaws.com:5432/dag3cksb6esvq'
