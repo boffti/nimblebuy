48b87e5c81104edd435e86973ecc0616c760b69f
Kruthika Krishnamurthy <boffti@gmail.com>
2020-04-12 19:41:46 +0530

    Updated Config URI

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///sfv_veggies.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DATABASE_URI = 'postgres://pxvtdswblsuiga:52cd888957385327482ddfeeb0ea6546a507abab0b46b3cd91505a3523dba853@ec2-18-235-97-230.compute-1.amazonaws.com:5432/dag3cksb6esvq'
+DATABASE_URI = 'postgres://hauqoqqdyajftq:28ca4ee1d28fba345ffcc74d6fc72804955eb4957ecc4d8f4b9cd742f1267dd0@ec2-3-234-109-123.compute-1.amazonaws.com:5432/df5gld6qit5fva'
