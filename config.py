8344657b8dd4a53e66faacc26ff811c271f93daa
Kruthika Krishnamurthy <boffti@gmail.com>
2020-04-12 19:48:51 +0530

    added env

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///sfv_veggies.db'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_URI = 'postgres://hauqoqqdyajftq:28ca4ee1d28fba345ffcc74d6fc72804955eb4957ecc4d8f4b9cd742f1267dd0@ec2-3-234-109-123.compute-1.amazonaws.com:5432/df5gld6qit5fva'
